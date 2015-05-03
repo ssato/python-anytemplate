1234e86d59998ad7a319c14b0ea0a40b2d6df9e4
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-03 09:35:29 +0900

    add common test module

---
+++ anytemplate/tests/common.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2015 Satoru SATOH <ssato at redhat.com>
+#
+import os.path
+import tempfile
+
+
+def selfdir():
+    return os.path.dirname(__file__)
+
+
+def setup_workdir():
+    return tempfile.mkdtemp(dir="/tmp", prefix="python-anytemplate-tests-")
+
+
+def cleanup_workdir(workdir):
+    """
+    FIXME: Danger!
+    """
+    os.system("rm -rf " + workdir)
+
+# vim:sw=4:ts=4:et:
