fe52e5d07cad72094d8059598c5ac0ae1111445f
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-03 09:30:44 +0900

    add some globals

---
+++ anytemplate/globals.py
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2012 - 2015 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+"""anytemplate globals.
+"""
+import logging
+
+try:
+    from logging import NullHandler
+except ImportError:  # python < 2.7 don't have it.
+    import logging
+
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+PACKAGE = "anytemplate"
+VERSION = "0.0.1"
+AUTHOR = "Satoru SATOH <ssat@redhat.com>"
+
+# See: "Configuring Logging for a Library" in python standard logging howto,
+# e.g. https://docs.python.org/2/howto/logging.html#library-config.
+LOGGER = logging.getLogger(PACKAGE)
+LOGGER.addHandler(NullHandler())
+
+# vim:sw=4:ts=4:et:
