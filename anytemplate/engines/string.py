2bc32ac5ce0f9e43222b56e5051e3adcd1910e71
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-03 12:11:15 +0900

    add string.Template (standard module) based template engine as a reference and its test cases

---
+++ anytemplate/engines/string.py
@@ -0,0 +1,65 @@
+#
+# Author: Satoru SATOH <ssato redhat.com>
+# License: BSD3
+#
+"""
+Standard string module based template engine in python dist.
+"""
+from __future__ import absolute_import
+
+import logging
+import string
+import anytemplate.engines.base
+import anytemplate.compat
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class StringTemplateEngine(anytemplate.engines.base.BaseEngine):
+
+    _name = "string"
+    _supported = True
+
+    def renders_impl(self, template_content, context=None, at_safe=False,
+                     **kwargs):
+        """
+        Inherited class must implement this!
+
+        :param template_content: Template content
+        :param context: A dict or dict-like object to instantiate given
+            template file
+        :param at_safe: Try to render template[s] safely, that is,
+            it will not raise any exceptions and returns the content of
+            template file itself if any error occurs
+        :param kwargs: Keyword arguments passed to the template engine to
+            render templates with specific features enabled.
+
+        :return: To be rendered string in inherited classes
+        """
+        if at_safe:
+            return string.Template(template_content).safe_substitute(context)
+        else:
+            return string.Template(template_content).substitute(context)
+
+    def render_impl(self, template, context=None, at_safe=False,
+                    at_encoding=anytemplate.compat.ENCODING, **kwargs):
+        """
+        Inherited class must implement this!
+
+        :param template: Template file path
+        :param context: A dict or dict-like object to instantiate given
+            template file
+        :param at_safe: Try to render template[s] safely, that is,
+            it will not raise any exceptions and returns the content of
+            template file itself if any error occurs
+        :param at_encoding: Template encoding
+        :param kwargs: Keyword arguments passed to the template engine to
+            render templates with specific features enabled.
+
+        :return: To be rendered string in inherited classes
+        """
+        with anytemplate.compat.copen(template, encoding=at_encoding) as tmpl:
+            return self.renders_impl(tmpl.read(), context, at_safe)
+
+# vim:sw=4:ts=4:et:
---
+++ anytemplate/engines/tests/string.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2015 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+import os.path
+import unittest
+import anytemplate.engines.string as TT
+import anytemplate.tests.common
+
+
+class Test_00(unittest.TestCase):
+
+    def test_20_renders_impl(self):
+        engine = TT.StringTemplateEngine()
+
+        trs = (("aaa", None, "aaa"), ("$a", {'a': "aaa"}, "aaa"))
+        for (tmpl_s, ctx, exp) in trs:
+            self.assertEquals(engine.renders_impl(tmpl_s, ctx), exp)
+
+
+class Test_10(unittest.TestCase):
+
+    def setUp(self):
+        self.workdir = anytemplate.tests.common.setup_workdir()
+
+    def tearDown(self):
+        if os.path.exists(self.workdir):
+            anytemplate.tests.common.cleanup_workdir(self.workdir)
+
+    def test_20_renders_impl(self):
+        engine = TT.StringTemplateEngine()
+
+        trs = (("aaa", None, "aaa"), ("$a", {'a': "aaa"}, "aaa"))
+        for (tmpl_s, ctx, exp) in trs:
+            tmpl = os.path.join(self.workdir, "test.tmpl")
+            open(tmpl, 'w').write(tmpl_s)
+
+            self.assertEquals(engine.render_impl(tmpl, ctx), exp)
+
+# vim:sw=4:ts=4:et:
