c164acedf4264498d7215186c9bf70a5078c811f
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-12 23:29:58 +0900

    refactor: extract common parts in anytemplate.api.render{s,} to anytemplate.api.__render

--- anytemplate/api.py
+++ anytemplate/api.py
@@ -62,6 +62,55 @@ def find_engine(filepath=None, name=None):
         return engine
 
 
+def __render(template=None, filepath=None, context=None, at_paths=None,
+             at_encoding=anytemplate.compat.ENCODING, at_engine=None,
+             at_ask_missing=False, at_cls_args=None, **kwargs):
+    """
+    Compile and render given template string and return the result string.
+
+    :param template: Template content string or None
+    :param filepath: Template file path or None
+    :param context: A dict or dict-like object to instantiate given
+        template file
+    :param at_paths: Template search paths
+    :param at_encoding: Template encoding
+    :param at_engine: Specify the name of template engine to use explicitly or
+        None to find it automatically anyhow.
+    :param at_cls_args: Arguments passed to instantiate template engine class
+    :param kwargs: Keyword arguments passed to the template engine to
+        render templates with specific features enabled.
+
+    :return: Rendered string
+    """
+    ecls = find_engine(filepath, at_engine)
+    LOGGER.info("Use the template engine: %s", ecls.name())
+    engine = ecls() if at_cls_args is None else ecls(**at_cls_args)
+    at_paths = anytemplate.utils.mk_template_paths(filepath, at_paths)
+
+    if filepath is None:
+        (render_fn, target) = (engine.renders, template)
+    else:
+        (render_fn, target) = (engine.render, filepath)
+
+    try:
+        return render_fn(target, context=context, at_paths=at_paths,
+                         at_encoding=at_encoding, **kwargs)
+    except anytemplate.engine.TemplateNotFound:
+        if not at_ask_missing:
+            raise
+
+        usr_tmpl = anytemplate.compat.raw_input(
+            "\n*** Missing template included. Please enter absolute or "
+            "relative path starting from '.' to the template file: "
+        )
+        usr_tmpl = anytemplate.utils.normpath(usr_tmpl.strip())
+        usr_tmpldir = os.path.dirname(usr_tmpl)
+
+        return render_fn(target, context=context,
+                         at_paths=(at_paths + [usr_tmpldir]),
+                         at_encoding=at_encoding, **kwargs)
+
+
 def renders(template_content, context=None, at_paths=None,
             at_encoding=anytemplate.compat.ENCODING,
             at_engine=None, at_ask_missing=False,
@@ -82,29 +131,10 @@ def renders(template_content, context=None, at_paths=None,
 
     :return: Rendered string
     """
-    ecls = find_engine(None, at_engine)
-    LOGGER.info("Use the template engine: %s", ecls.name())
-    engine = ecls() if at_cls_args is None else ecls(**at_cls_args)
-    at_paths = anytemplate.utils.mk_template_paths(None, at_paths)
-
-    try:
-        return engine.renders(template_content, context=context,
-                              at_paths=at_paths, at_encoding=at_encoding,
-                              **kwargs)
-    except anytemplate.engine.TemplateNotFound:
-        if not at_ask_missing:
-            raise
-
-        usr_tmpl = anytemplate.compat.raw_input(
-            "\n*** Missing template included. Please enter absolute or "
-            "relative path starting from '.' to the template file: "
-        )
-        usr_tmpl = anytemplate.utils.normpath(usr_tmpl.strip())
-        usr_tmpldir = os.path.dirname(usr_tmpl)
-
-        return engine.renders(template_content, context=context,
-                              at_paths=(at_paths + [usr_tmpldir]),
-                              at_encoding=at_encoding, **kwargs)
+    return __render(template_content, context=context, at_paths=at_paths,
+                    at_encoding=at_encoding, at_engine=at_engine,
+                    at_ask_missing=at_ask_missing, at_cls_args=at_cls_args,
+                    **kwargs)
 
 
 def render(filepath, context=None, at_paths=None,
@@ -127,35 +157,10 @@ def render(filepath, context=None, at_paths=None,
 
     :return: Rendered string
     """
-    ecls = find_engine(filepath, at_engine)
-    LOGGER.info("Use the template engine: %s", ecls.name())
-    engine = ecls() if at_cls_args is None else ecls(**at_cls_args)
-
-    if at_paths is None:
-        at_paths = anytemplate.utils.mk_template_paths(filepath, at_paths)
-    try:
-        return engine.render(filepath, context=context, at_paths=at_paths,
-                             at_encoding=at_encoding, **kwargs)
-    except anytemplate.engine.TemplateNotFound:
-        if not at_ask_missing:
-            raise
-
-        usr_tmpl = anytemplate.compat.raw_input(
-            "\n*** Missing template '%s' or any/one of templates included "
-            "from %s. Please enter absolute or relative path starting "
-            "from '.' to the template file: " % (filepath, filepath)
-        )
-        usr_tmpl = anytemplate.utils.normpath(usr_tmpl.strip())
-        usr_tmpldir = os.path.dirname(usr_tmpl)
-
-        if os.path.basename(filepath) == os.path.basename(usr_tmpl):
-            tmpl = usr_tmpl
-        else:
-            tmpl = filepath
-
-        return engine.render(tmpl, context=context,
-                             at_paths=(at_paths + [usr_tmpldir]),
-                             at_encoding=at_encoding, **kwargs)
+    return __render(filepath=filepath, context=context, at_paths=at_paths,
+                    at_encoding=at_encoding, at_engine=at_engine,
+                    at_ask_missing=at_ask_missing, at_cls_args=at_cls_args,
+                    **kwargs)
 
 
 def render_to(filepath, context=None, output=None, at_paths=None,
