6673c699b9857fa8cc1418ac25ab6e6a3b8b7fd8
Satoru SATOH <satoru.satoh@gmail.com>
2015-11-23 10:58:36 +0900

    refactor: remove extra try-import hack in .engines.tests.cheetah

--- anytemplate/engines/tests/cheetah.py
+++ anytemplate/engines/tests/cheetah.py
@@ -7,21 +7,13 @@ import os
 import unittest
 
 import anytemplate.tests.common
-
-try:
-    import anytemplate.engines.cheetah as TT
-    try:
-        import Cheetah.Template  # flake8: noqa
-    except ImportError:
-        TT = None  # flake8: noqa
-except ImportError:
-    TT = None
+import anytemplate.engines.cheetah as TT
 
 
 class Test(unittest.TestCase):
 
     def test_12__init__w_kwargs(self):
-        if TT is not None:
+        if TT.Template is not None:
             self.assertTrue(isinstance(TT.Engine(errorCatcher=None),
                                        TT.Engine))
 
@@ -29,40 +21,40 @@ class Test(unittest.TestCase):
         tmpl_s = "hello, $name!"
         exp = "hello, John!"
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             self.assertEquals(egn.renders(tmpl_s, {"name": "John", }), exp)
 
     def test_22_renders__no_context(self):
         tmpl_s = "hello world!"
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             self.assertEquals(egn.renders(tmpl_s), tmpl_s)
 
     def test_24_renders__no_context__w_at_path(self):
         tmpl_s = "hello world!"
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             self.assertEquals(egn.renders(tmpl_s, at_paths=['.']), tmpl_s)
 
     def test_26_renders__no_context__w_file(self):
         tmpl_s = "hello world!"
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             self.assertEquals(egn.renders(tmpl_s, file="x"), tmpl_s)
 
     def test_28_renders__with_engine_special_option(self):
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine(compilerSettings={'cheetahVarStartToken': '@'})
             self.assertEquals(egn.renders("@a", {'a': ""}), '')
 
     def test_36_renders_impl__no_context__w_filename(self):
         tmpl_s = "hello world!"
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             self.assertEquals(egn.renders_impl(tmpl_s, {}, file="x"),
                               tmpl_s)
@@ -82,7 +74,7 @@ class Test10(unittest.TestCase):
         ctx = dict(greeting="hello, Cheetah!", )
         open(tmpl, 'w').write(tmpl_s)
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             res = egn.render(tmpl, ctx, at_paths=[self.workdir])
             self.assertEquals(res, ctx["greeting"])
@@ -92,7 +84,7 @@ class Test10(unittest.TestCase):
         tmpl_s = "$getVar('greeting', 'hello!')"
         open(tmpl, 'w').write(tmpl_s)
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             res = egn.render(tmpl)
             self.assertEquals(res, "hello!")
@@ -103,7 +95,7 @@ class Test10(unittest.TestCase):
         ctx = dict(greeting="hello, Cheetah!", )
         open(tmpl, 'w').write(tmpl_s)
 
-        if TT is not None:
+        if TT.Template is not None:
             egn = TT.Engine()
             res = egn.render_impl(tmpl, ctx, at_paths=[self.workdir],
                                   source="aaa")
