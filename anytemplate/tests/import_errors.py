d96be17eb6b05adeb278591eeabbbb47598c8a11
Satoru SATOH <satoru.satoh@gmail.com>
2016-11-08 09:17:09 +0900

    enhancement: add some more test cases to check import errors

--- anytemplate/tests/import_errors.py
+++ anytemplate/tests/import_errors.py
@@ -17,4 +17,12 @@ class Test(unittest.TestCase):
         self.assertFalse(cls in globals())
         self.assertFalse(getattr(anytemplate.globals, cls) is None)
 
+    def test_20_engines(self):
+        for mod in ("Cheetah", "jinja2", "mako", "tenjin", "pystache"):
+            sys.modules[mod] = None
+            import anytemplate.engine
+
+            self.assertTrue(sys.modules[mod] is None)
+            self.assertFalse(anytemplate.engine is None)
+
 # vim:sw=4:ts=4:et:
