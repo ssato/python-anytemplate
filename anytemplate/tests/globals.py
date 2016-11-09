5ab078bb7237d93a15de3d7f4c959146ee246a29
Satoru SATOH <satoru.satoh@gmail.com>
2016-11-09 21:48:55 +0900

    refactor: add a (dummy) test case of logging handler's emitter

--- anytemplate/tests/globals.py
+++ anytemplate/tests/globals.py
@@ -12,6 +12,7 @@ class Test(unittest.TestCase):
     def test_10_null_handler(self):
         logger = TT.logging.getLogger(__name__)
         logger.addHandler(TT.NullHandler_())
+        logger.debug("aaa")
 
     def test_20_logger(self):
         TT.LOGGER.info("test log")
