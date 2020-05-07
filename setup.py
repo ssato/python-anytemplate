715c80c2fefa4de22b10970f721f802aa2f5dd75
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-07 09:48:30 +0900

    fix/change: drop py2 support in RPMs and cleanup/fix packaging stuff
    - setup.py: simplify the version detection code, revision support in the
      RELEASE tag in the RPM SPEC, remove unused import (sys).
    - setup.cfg: add project_urls metadata to be shown in the PyPI page,
      drop python 3.3 .. 3.5 and add python 3.8 support, add minimum install
      dependencies, remove unsed pytest stuff
    - pkg/package.spec.in: use Release tag instead of Version tag for
      snapshot versioning mode, drop py2 support and related hacks

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
+"""setup.py to build package.
+"""
 from __future__ import absolute_import
 
+import glob
 import os.path
 import os
 import re
-import sys
 import setuptools
 import setuptools.command.bdist_rpm
 
 
 # It might throw IndexError and so on.
 VERSION = [re.search(r'^VERSION = "([^"]+)"', l).groups()[0] for l
-           in open("src/anytemplate/globals.py").readlines()
+           in open(glob.glob("src/*/globals.py")[0])
            if "VERSION" in l][0]
 
 # For daily snapshot versioning mode:
+RELEASE = "1%{?dist}"
 if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
     import datetime
-    VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
+    RELEASE = datetime.datetime.now().strftime(".%Y%m%d%H%M%S")
+
+
+def _replace(line):
+    """Replace some strings in the RPM SPEC template"""
+    if "@VERSION@" in line:
+        return line.replace("@VERSION@", VERSION)
+
+    if "@RELEASE@" in line:
+        return line.replace("@RELEASE@", RELEASE)
+
+    if "Source0:" in line:  # Dirty hack
+        return "Source0: %{pkgname}-%{version}.tar.gz"
+
+    return line
 
 
 class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
@@ -25,23 +42,13 @@ class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
     spec_tmpl = os.path.join(os.path.abspath(os.curdir),
                              "pkg/package.spec.in")
 
-    def _replace(self, line):
-        """Replace some strings in the RPM SPEC template"""
-        if "@VERSION@" in line:
-            return line.replace("@VERSION@", VERSION)
-
-        if "Source0:" in line:  # Dirty hack
-            return "Source0: %{pkgname}-%{version}.tar.gz"
-
-        return line
-
     def _make_spec_file(self):
-        return [self._replace(l.rstrip()) for l
-                in open(self.spec_tmpl).readlines()]
+        return [_replace(l.rstrip()) for l in open(self.spec_tmpl)]
 
 
-setuptools.setup(name="anytemplate", version=VERSION,
-                 package_dir={'': 'src'},
-                 cmdclass=dict(bdist_rpm=bdist_rpm))
+setuptools.setup(name="anytemplate",   # Avoid 'Unknown' package in older ones.
+                 version=VERSION,
+                 cmdclass=dict(bdist_rpm=bdist_rpm),
+                 package_dir={'': 'src'})
 
 # vim:sw=4:ts=4:et:
