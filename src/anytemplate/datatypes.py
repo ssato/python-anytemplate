49a60812472381a6df5805aa792c22e8f824fdc1
Satoru SATOH <satoru.satoh@gmail.com>
2024-07-14 00:10:48 +0900

    enhancement: add to consoidate type defs

---
+++ src/anytemplate/datatypes.py
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2024 Satoru SATOH <satoru.satoh gmail.com>
+# License: MIT
+#
+"""Data types used internally.
+
+The module imports this should have the line like as follows.
+
+.. code-block:: pytohn
+
+   import typing
+
+   if typing.TYPE_CHECKING:
+       from .datatypes import (
+           PathType, MaybePath, MaybeCtx
+       )
+"""
+from __future__ import annotations
+
+import collections.abc
+import typing
+
+
+PathType = str  # It will be typing.Union[str, pathlib.Path].
+MaybePath = typing.Optional[PathType]
+MaybeCtx = typing.Optional[collections.abc.Mapping]
