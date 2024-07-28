c8dee617b94e259b6b1a775b07ddb5eacb22b770
Satoru SATOH <satoru.satoh@gmail.com>
2024-07-28 13:02:11 +0900

    fix: replace call of subprocess.check_output with subprocess.run

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,7 @@
 """
 from __future__ import absolute_import
 
+import json
 import subprocess
 
 import pytest
@@ -86,7 +87,7 @@ def test_run_main__jinja2(tmp_path):
     assert out.read_text() == "hello"
 
 
-def _subproc_check_out(cmd_str, request):
+def _subproc_run(args, request):
     """Call subprocess.check_output with some keyword arguments.
 
     .. seealso::
@@ -95,41 +96,43 @@ def _subproc_check_out(cmd_str, request):
     """
     src_root = request.path.parent.parent.absolute()
 
-    cmd = "python3 src/anytemplate/cli.py"
-    opts = {
-        "env": {"PYTHONPATH": "src"},
-        "shell": True,
-        "cwd": str(src_root)
-    }
-
-    return subprocess.check_output(cmd_str.replace("CMD", cmd), **opts)
+    return subprocess.run(
+        ["python3", "src/anytemplate/cli.py", *args],
+        env={"PYTHONPATH": "src"},
+        shell=True,
+        cwd=str(src_root),
+        capture_output=True,
+        text=True,
+        check=True,
+    )
 
 
 @pytest.mark.parametrize(
-    ("tmpl_s", "ctx_s", "exp"),
-    (("a\n", '{}', "a"),
-     ("$a\n", '{"a": "aaa"}', "aaa"),
+    ("tmpl_s", "ctx", "exp"),
+    (("a\n", {}, "a"),
+     ("$a\n", {"a": "aaa"}, "aaa"),
      )
 )
 def test_strtemplate_with_ctx(
-    tmpl_s, ctx_s, exp, tmp_path, request
+    tmpl_s, ctx, exp, tmp_path, request
 ):
     tmpl = tmp_path / "test.tmpl"
     tmpl.write_text(tmpl_s)
 
-    ctx = tmp_path / "ctx.json"
-    ctx.write_text(ctx_s)
+    cpath = tmp_path / "ctx.json"
+    with cpath.open(mode="w", encoding="utf-8") as ctxf:
+        json.dump(ctx, ctxf)
 
     try:
-        out = _subproc_check_out(
-            # TBD: Read ctx from stdin.
-            # f"echo 'a: aaa' | CMD -E string.Template -C yaml:- -o - {tmpl}",
-            f"CMD -E string.Template -C json:{ctx} -o - {tmpl}",
+        res = _subproc_run(
+            ["-E", "string.Template", "-C", f"json:{cpath!s}",
+             "-o", "-", str(tmpl)],
             request
         )
     except (IOError, OSError, CompileError):
         print(f"tmpl: {tmpl.read_text()}")
-        print(f"ctx: {ctx.read_text()}")
+        print(f"ctx: {cpath.read_text()}")
         raise
 
-    assert out.rstrip() == bytes(exp, "utf-8")
+    assert not res.stderr
+    assert res.stdout.rstrip() == exp
